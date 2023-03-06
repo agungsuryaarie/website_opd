e04262049f5c00a08ec74ecf28436ce061cb9a4b
agungsuryaarie <agungbahari3007@gmail.com>
2023-03-07 01:42:44 +0700

    add

--- resources/views/post_pemerintahan.blade.php
+++ resources/views/post_pemerintahan.blade.php
@@ -5,21 +5,25 @@
         <div class="section-tittle">
             <h3>{{ $title2 }}</h3>
         </div>
-        <div class="row-card">
-            @foreach ($post_pemerintahan as $pp)
-                <div class="cardpokersize">
-                    <img src="{{ url('storage/berita/' . $pp->foto) }}">
-                    <ul class="blog-info">
-                        <li><i class="bi bi-calendar"></i> {{ $pp->tanggal }}</li>
-                        <li><i class="bi bi-clock"></i> {{ $pp->jam }} WIB</li>
-                    </ul>
-                    <h3><a href="{{ route('post.show', $pp->slug) }}">{{ $pp->judul }}</a></h3>
-                    <p>Input data perencanaan lima tahunan yang dilakukan oleh fungsi perencana pada Pemerintah
-                        Daerah
-                    </p>
-                </div>
-            @endforeach
-            {{ $post_pemerintahan->links() }}
+        <div class="container-card">
+            <div class="row-card">
+                @foreach ($post_pemerintahan as $pp)
+                    <div class="cardpokersize">
+                        <img src="{{ url('storage/berita/' . $pp->foto) }}">
+                        <ul class="blog-info">
+                            <li><i class="bi bi-calendar"></i> {{ $pp->tanggal }}</li>
+                            <li><i class="bi bi-clock"></i> {{ $pp->jam }} WIB</li>
+                        </ul>
+                        <h3><a href="{{ route('post.show', $pp->slug) }}">{{ $pp->judul }}</a></h3>
+                        <p>Input data perencanaan lima tahunan yang dilakukan oleh fungsi perencana pada Pemerintah
+                            Daerah
+                        </p>
+                    </div>
+                @endforeach
+            </div>
+            <div class="d-flex justify-center mb-30">
+                {{ $post_pemerintahan->links('pagination::bootstrap-4') }}
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/post_umum.blade.php
+++ resources/views/post_umum.blade.php
@@ -5,21 +5,25 @@
         <div class="section-tittle">
             <h3>{{ $title2 }}</h3>
         </div>
-        <div class="row-card">
-            @foreach ($post_umum as $pu)
-                <div class="cardpokersize">
-                    <img src="{{ url('storage/berita/' . $pu->foto) }}">
-                    <ul class="blog-info">
-                        <li><i class="bi bi-calendar"></i> {{ $pu->tanggal }}</li>
-                        <li><i class="bi bi-clock"></i> {{ $pu->jam }} WIB</li>
-                    </ul>
-                    <h3><a href="{{ route('post.show', $pu->slug) }}">{{ $pu->judul }}</a></h3>
-                    <p>Input data perencanaan lima tahunan yang dilakukan oleh fungsi perencana pada Pemerintah
-                        Daerah
-                    </p>
-                </div>
-            @endforeach
-            {{ $post_umum->links() }}
+        <div class="container-card">
+            <div class="row-card">
+                @foreach ($post_umum as $pu)
+                    <div class="cardpokersize">
+                        <img src="{{ url('storage/berita/' . $pu->foto) }}">
+                        <ul class="blog-info">
+                            <li><i class="bi bi-calendar"></i> {{ $pu->tanggal }}</li>
+                            <li><i class="bi bi-clock"></i> {{ $pu->jam }} WIB</li>
+                        </ul>
+                        <h3><a href="{{ route('post.show', $pu->slug) }}">{{ $pu->judul }}</a></h3>
+                        <p>Input data perencanaan lima tahunan yang dilakukan oleh fungsi perencana pada Pemerintah
+                            Daerah
+                        </p>
+                    </div>
+                @endforeach
+            </div>
+            <div class="d-flex justify-center mb-30">
+                {{ $post_umum->links('pagination::bootstrap-4') }}
+            </div>
         </div>
     </div>
 @endsection
