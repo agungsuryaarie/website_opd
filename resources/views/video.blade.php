9fc1c82b1e9eae227c29c3e3bbca6c0c06ace5fa
abiihutabarat29 <abiihutabarat97@gmail.com>
2023-03-06 22:35:37 +0700

    parsing link video

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -5,8 +5,10 @@
             <div class="row-image">
                 @foreach ($video as $vid)
                     <div class="card-image">
-                        {{ preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/", $vid->link, $matches) }}
-                        <img src="">
+                        {{ preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $vid->link, $match) }}
+                        {{-- Video old {{ preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/", $vid->link, $matches) }} --}}
+                        <iframe src="https://www.youtube.com/embed/{{ $match[1] }}" allowfullscreen
+                            class="absolute top-0 left-0 w-full h-full rounded-xl" title="Video"></iframe>
                         <a href="">
                             <h3>{{ $vid->judul }}</h3>
                         </a>
